75bdaf558a78f1beb10e86a22dfd427ac84310e9
MaryEddythe <sornito.maryeddythe@gmail.com>
2025-05-03 21:34:01 +0800

    removed gesture delay

--- slide.py
+++ slide.py
@@ -12,8 +12,8 @@ prev_x = 0
 prev_y = 0
 last_gesture_time = time.time()
 
-horizontal_threshold = 20 
-vertical_threshold = 20    
+horizontal_threshold = 10
+vertical_threshold = 10
 
 while True:
     success, img = cap.read()
@@ -28,29 +28,30 @@ while True:
             x = int(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x * img.shape[1])
             y = int(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y * img.shape[0])
 
-            if time.time() - last_gesture_time > gesture_delay:
-                dx = x - prev_x
-                dy = y - prev_y
+            dx = x - prev_x
+            dy = y - prev_y
 
+            if time.time() - last_gesture_time:
                 if dx > horizontal_threshold:
-                    pyautogui.hotkey('ctrl', 'pagedown')  
+                    pyautogui.hotkey('ctrl', 'pagedown')
                     print("Next Slide")
                     last_gesture_time = time.time()
                 elif dx < -horizontal_threshold:
-                    pyautogui.hotkey('ctrl', 'pageup')  
+                    pyautogui.hotkey('ctrl', 'pageup')
                     print("Previous Slide")
                     last_gesture_time = time.time()
                 elif dy > vertical_threshold:
-                    pyautogui.scroll(500)  
+                    pyautogui.scroll(500)
                     print("Scroll Up")
                     last_gesture_time = time.time()
                 elif dy < -vertical_threshold:
-                    pyautogui.scroll(-500)  
+                    pyautogui.scroll(-500)
                     print("Scroll Down")
                     last_gesture_time = time.time()
 
-            prev_x = x
-            prev_y = y
+            if abs(dx) > horizontal_threshold or abs(dy) > vertical_threshold:
+                prev_x = x
+                prev_y = y
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == 27:
